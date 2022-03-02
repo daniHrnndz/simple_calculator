7a0008909837c6c698df921f7db7202ed85bd7a0
Daniel Hernandez <dan.alejandro.hrnndz@gmail.com>
2022-03-02 20:08:33 -0300

    CSS & JS
    CSS almost finished, JS just started

---
+++ script.js
@@ -0,0 +1,20 @@
+class Calculator {
+    constructor(calcDisplay) {
+        this.calcDIsplay = calcDisplay
+    }
+}
+
+const calcDisplay = document.querySelector(".calc_display");
+
+
+const numberBtn = document.querySelectorAll(".calc_btn-num");
+const operatorBtn = document.querySelectorAll(".calc_btn-operator");
+const clearBtn = document.querySelector(".calc_btn-clear");
+const equalBtn = document.querySelector(".calc_btn-equal");
+
+console.log(numberBtn)
+console.log(operatorBtn)
+console.log(clearBtn)
+console.log(equalBtn)
+console.log(calcDisplay)
+
